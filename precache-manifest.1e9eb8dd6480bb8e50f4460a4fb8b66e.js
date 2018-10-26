020d6a68bd4759969f364b2cf874a61baa691412
andres9722 <andres_9722@outlook.com>
2018-10-26 16:21:54 -0500

    Updates

---
+++ precache-manifest.1e9eb8dd6480bb8e50f4460a4fb8b66e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0555af0d0302f662e6a",
+    "url": "/react-hooks/static/js/runtime~main.c0555af0.js"
+  },
+  {
+    "revision": "16876b2251aafade311a",
+    "url": "/react-hooks/static/js/main.16876b22.chunk.js"
+  },
+  {
+    "revision": "c2f2fe0f0e6b16477a10",
+    "url": "/react-hooks/static/js/1.c2f2fe0f.chunk.js"
+  },
+  {
+    "revision": "16876b2251aafade311a",
+    "url": "/react-hooks/static/css/main.109d9ca7.chunk.css"
+  },
+  {
+    "revision": "150fa0685511c0335a80aeaef2158ed3",
+    "url": "/react-hooks/index.html"
+  }
+];
