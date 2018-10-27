9a88811eb7fe93b5aa3f8f6355a7c54faf92b7cf
andres9722 <andres_9722@outlook.com>
2018-10-27 09:27:37 -0500

    Adding styles

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,30 +2,28 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './Menu.scss'
 
-const Menu = () => {
-  return (
-    <nav className='nav'>
-      <ul className='menu'>
-        <li className='menu__item'>
-          <Link className='menu__link' to='/todos'>use state hook</Link>
-        </li>
-        <li className='menu__item'>
-          <Link className='menu__link' to='/state'>
-            state custom hooks
-          </Link>
-        </li>
-        <li className='menu__item'>
-          <Link className='menu__link' to='/effect'>use effect hook</Link>
-        </li>
-        <li className='menu__item'>
-          <Link className='menu__link' to='/memo'>react memo</Link>
-        </li>
-        <li className='menu__item'>
-          <Link className='menu__link' to='/lazy'>react lazy</Link>
-        </li>
-      </ul>
-    </nav>
-  )
-}
+const Menu = () => (
+  <nav className='nav'>
+    <ul className='menu'>
+      <li className='menu__item'>
+        <Link className='menu__link' to='/todos'>use state hook</Link>
+      </li>
+      <li className='menu__item'>
+        <Link className='menu__link' to='/state'>
+          state custom hooks
+        </Link>
+      </li>
+      <li className='menu__item'>
+        <Link className='menu__link' to='/effect'>use effect hook</Link>
+      </li>
+      <li className='menu__item'>
+        <Link className='menu__link' to='/memo'>react memo</Link>
+      </li>
+      <li className='menu__item'>
+        <Link className='menu__link' to='/lazy'>react lazy</Link>
+      </li>
+    </ul>
+  </nav>
+)
 
 export default Menu
